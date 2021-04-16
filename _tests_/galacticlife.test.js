588733d169e0644f4f5715958c3682ac00594828
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-16 09:34:53 -0700

    add better naming conventions to variables

--- _tests_/galacticlife.test.js
+++ _tests_/galacticlife.test.js
@@ -4,10 +4,10 @@ describe ('Converter', () => {
   
   test ('should correctly create age object', () => {
   const converter = new Converter(25,25,25,25);
-  expect (converter.conversion1).toEqual(25)
-  expect (converter.conversion2).toEqual(25)
-  expect (converter.conversion3).toEqual(25)
-  expect (converter.conversion4).toEqual(25)
+  expect (converter.mercurycon).toEqual(25)
+  expect (converter.venuscon).toEqual(25)
+  expect (converter.marscon).toEqual(25)
+  expect (converter.jupitercon).toEqual(25)
   });
 
 });
--- src/galacticlife.js
+++ src/galacticlife.js
@@ -1,13 +1,13 @@
 export default class Converter {
-  constructor (conversion1,conversion2,conversion3,conversion4) {
-    this.conversion1 = conversion1
-    this.conversion2 = conversion2
-    this.conversion3 = conversion3
-    this.conversion4 = conversion4
+  constructor (mercurycon,venuscon,marscon,jupitercon) {
+    this.mercurycon = mercurycon
+    this.venuscon = venuscon
+    this.marscon = marscon
+    this.jupitercon = jupitercon
 }
 
   // divideAge () {
-  //   this.mercury /= 25;
+  //   this.conversion1 /= 25;
   //   this.venus /= 25;
   //   this.mars /= 25;
   //   this.jupiter /= 25;
