ad3488f41adab3479c3323a6134ef72e0f63a679
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-16 09:40:54 -0700

    write second test for divideAge method

--- _tests_/galacticlife.test.js
+++ _tests_/galacticlife.test.js
@@ -2,12 +2,17 @@ import Converter from './../src/galacticlife.js'
 
 describe ('Converter', () => {
   
-  test ('should correctly create age object', () => {
+  test ('should correctly create converter class/object', () => {
   const converter = new Converter(25,25,25,25);
   expect (converter.mercurycon).toEqual(25)
   expect (converter.venuscon).toEqual(25)
   expect (converter.marscon).toEqual(25)
   expect (converter.jupitercon).toEqual(25)
   });
+
+  test ('should correctly divide by mercury planetary years and convert age', () => {
+    const converter = new Converter(25)
+    expect (converter.mercurycon).toEqual(104.16)
+  }
 
 });
