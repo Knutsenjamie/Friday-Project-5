e9736f1b2f54dd52e68d7f8c2387fcf6345e274a
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-21 14:07:59 -0700

    refactor and pass second test

--- _tests_/galacticlife.test.js
+++ _tests_/galacticlife.test.js
@@ -3,17 +3,18 @@ import Converter from './../src/galacticlife.js'
 describe ('Converter', () => {
   
   test ('should correctly create converter class/object', () => {
-  const converter = new Converter(0.24,0.62,1.88,11.86,25);
-  expect (converter.mercurycon).toEqual(0.24)
-  expect (converter.venuscon).toEqual(0.62)
-  expect (converter.marscon).toEqual(1.88)
-  expect (converter.jupitercon).toEqual(11.86)
+  const converter = new Converter(25);
   expect (converter.age).toEqual(25)
   });
 
-  test ('should correctly divide by planetary years and convert age', () => {
-    const converter = new Converter(0.24,0.62,1.88,11.86,25);
-    expect (converter.divideAge()).toEqual([[104.16666666666667, "Your Age On Mercury Is: 104"],[40.32258064516129, "Your Age On Venus Is: 40"],[13.297872340425533, "Your Age On Mars Is: 13"],[2.1079258010118043, "Your Age On Jupiter Is: 2"]])
+  test ('should correctly divide by Mercury planetary years and convert age', () => {
+    const converter = new Converter(25);
+    expect (converter.divideAgeMerc()).toEqual([104.16666666666667, "Your Age On Mercury Is: 104"])
+  });
+
+  test ('should correctly divide by Venus planetary years and convert age', () => {
+    const converter = new Converter(25);
+    expect (converter.divideAgeVenus()).toEqual([40.32258064516129, "Your Age On Venus Is: 40"])
   });
 
 });
--- src/galacticlife.js
+++ src/galacticlife.js
@@ -1,18 +1,11 @@
 export default class Converter {
-  constructor (mercurycon,venuscon,marscon,jupitercon,age) {
-    this.mercurycon = mercurycon;
-    this.venuscon = venuscon;
-    this.marscon = marscon;
-    this.jupitercon = jupitercon
+  constructor (age) {
     this.age = age;
     }
 
-  divideAge () {
-    this.age /= this.mercurycon;
-    this.age /= this.venuscon;
-    this.age /= this.marscon;
-    this.age /= this.jupitercon;
-    return ([[this.age, "Your Age On Mercury Is: 104"], [this.age, "Your Age On Venus Is: 40"], [this.age, "Your Age On Mars Is: 13"], [this.age, "Your Age On Jupiter Is: 2"]]);
+  divideAgeMerc () {
+    this.age /= 0.24;
+    return ([this.age, "Your Age On Mercury Is: 104"]);
   }
 
 }
