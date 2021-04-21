3f992485469a7c815fac33db0be015f797f9abaa
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-21 12:00:48 -0700

    WIP: attempt to refactor divideAge

--- src/galacticlife.js
+++ src/galacticlife.js
@@ -1,18 +1,18 @@
 export default class Converter {
-  constructor (mercurycon,venuscon,marscon,jupitercon) {
-    this.mercurycon = 0.24;
-    this.venuscon = 0.62;
-    this.marscon = 1.88;
-    this.jupitercon = 11.86
-    this.age = {};
+  constructor (mercurycon,venuscon,marscon,jupitercon,age) {
+    this.mercurycon = mercurycon;
+    this.venuscon = venuscon;
+    this.marscon = marscon;
+    this.jupitercon = jupitercon
+    this.age = age;
     }
 
   divideAge () {
-    this.mercurycon /= this.age;
-    this.venuscon /= this.age;
-    this.marscon /= this.age;
-    this.jupitercon /= this.age;
-    return ([[this.age, "Your Age On Mercury Is: 104"], [this.age, "Your Age On Venus Is: 40"], [this.marscon, "Your Age On Mars Is: 13"], [this.jupitercon, "Your Age On Jupiter Is: 2"]]);
+    this.age /= this.mercurycon;
+    this.age /= this.venuscon;
+    this.age /= this.marscon;
+    this.age /= this.jupitercon;
+    return ([[this.age, "Your Age On Mercury Is: 104"], [this.age, "Your Age On Venus Is: 40"], [this.age, "Your Age On Mars Is: 13"], [this.age, "Your Age On Jupiter Is: 2"]]);
   }
 
 }
