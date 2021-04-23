297107b2e093ec34976dab4c234e2283911ca97b
Knutsenjamie <Knutsenjamie@yahoo.com>
2021-04-22 20:31:04 -0700

    revert code back to original on galacticexpectancy test and scripts page

--- _tests_/galacticexpectancy.test.js
+++ _tests_/galacticexpectancy.test.js
@@ -1,20 +1,16 @@
 import Expector from './../src/galacticexpectancy'
-
-
 describe ('Expector', () => {
   
   test ('should correctly create expector class/object', () => {
-  const expector = new Expector(25, 78.54);
-  expect (expector.age).toEqual(25)
-  expect (expector.earthLifeExpectancy).toEqual(78.54)
+  const expector = new Expector(78.54,78.54,78.54,78.54);
+  expect (expector.mercuryexpect).toEqual(78.54)
+  expect (expector.venusexpect).toEqual(78.54)
+  expect (expector.marsexpect).toEqual(78.54)
+  expect (expector.jupiterexpect).toEqual(78.54)
   });
   
-  test ('should correctly subtract a users age by the life expectancy on Earth', () => {
-    const expector = new Expector(78.54,25);
-    expect (expector.lifeExpectEquation()).toEqual(53.540000000000006)
-  });
-
-  test ('should correctly return users life expectancy on all planets', () => {
-    const expector = new Expector (78.54,25)
+  test ('should correctly return users life expectancy on all planets', () => { 
+    const expector = new Expector (53.54,53.54,53.54,53.54)
+    expect (expector.lifeExpect()).toEqual([[144.54333333333335, "You'd still live about 145 more years on Mercury!"], [7.814838709677417, "You'd still live about 8 more years on Venus!"], [-50.06127659574469, "-50? Sorry, you'd already be dead!"], [-74.02566610455312, "-74? Sorry, you'd already be dead!"]])
   });
 });
--- src/galacticexpectancy.js
+++ src/galacticexpectancy.js
@@ -1,14 +1,22 @@
 export default class Expector {
-  constructor (age, earthLifeExpectancy) {
-    this.age = age;
-    this.earthLifeExpectancy = earthLifeExpectancy;
-  }
-
-  lifeExpectEquation() {
-  return this.age -= this.earthLifeExpectancy 
-  }
-
+  constructor (mercuryexpect,venusexpect,marsexpect,jupiterexpect) {
+    this.mercuryexpect = mercuryexpect
+    this.venusexpect = venusexpect
+    this.marsexpect = marsexpect
+    this.jupiterexpect = jupiterexpect
   
+  }
+  
+  lifeExpect () {
+    this.mercuryexpect /= 0.24;
+    this.venusexpect /= 0.62;
+    this.marsexpect /= 1.88;
+    this.jupiterexpect /= 11.86
+    return ([[this.mercuryexpect -= 78.54, "You'd still live about 145 more years on Mercury!"], [this.venusexpect -= 78.54, "You'd still live about 8 more years on Venus!"], [this.marsexpect -= 78.54, "-50? Sorry, you'd already be dead!"], [this.jupiterexpect -= 78.54, "-74? Sorry, you'd already be dead!"]]);
+  }
+
+
+
 }
 
 
